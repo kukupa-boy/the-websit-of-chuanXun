f35c4219b2cea6332a34adfa0d7ac783cfcdff8a
yan <1544566201@qq.com>
2019-09-24 11:46:52 +0800

    chuanxun

---
+++ src/main.js
@@ -0,0 +1,50 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+
+// ----------------------------------------------element.js配置---------------------------------------------------------
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+Vue.use(ElementUI)
+
+// -------------------------------------------字体库font-awesome的配置--------------------------------------------------
+import 'font-awesome/css/font-awesome.css'
+Vue.use(animate)
+
+//-------------------------------------------组件切换动画的配置---------------------------------------------------------
+import animate from 'animate.css'
+
+
+//-------------------------------------------Vuex数据共享的配置---------------------------------------------------------
+import vuex from 'vuex'
+Vue.use(vuex)
+import store from './vuex/store.js'
+
+
+//-------------------------------------------axios获取数据的配置--------------------------------------------------------
+import  Axios from "axios";
+
+//------------------------------------------vue实现跨域（反向代理）-----------------------------------------------------
+Vue.prototype.HOST='/api'
+Vue.prototype.axios=Axios;
+
+
+//----------------------------------------------使用jquery的配置--------------------------------------------------------
+
+
+
+
+
+Vue.config.productionTip = false
+
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  store,
+  components: { App },
+  template: '<App/>'
+})
---
+++ src/vuex/store.js
@@ -0,0 +1,21 @@
+
+import Vue from 'vue'
+import Vuex from 'vuex'
+Vue.use(Vuex)
+//state对象主要是储存数据
+  var state={
+    headerName:"search.vue",
+    screenHeight:""
+  }
+//mutations主要是为了该bainstate里面的数据值
+  var mutations={
+    changeHeadName(state,name){//第一个对象必须是state，你也可以写这个参数，但是第一个参数必须是state
+      state.headerName=name
+    }
+  }
+//实例化vuex.store
+const store=new Vuex.Store({
+  state,
+  mutations
+})
+export default  store;
